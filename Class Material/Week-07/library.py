2a543adec187b574ddaf17896c676ad3d3fe6d10
Clariza Mayo <clarizamayo@Clarizas-Air.cable.rcn.com>
2020-10-29 23:13:39 -0400

    week 7 lab 1 and project 6

---
+++ Class Material/Week-07/library.py
@@ -0,0 +1,16 @@
+def hello_world():  #this will be called if we use python
+    print("hello world")
+
+def func(arg):
+    return 100 * arg
+
+if __name__ == "__main__":  # the content here will be the part that will be executed if we do it in the terminal
+    print(func(100))
+    hello_world()
+
+
+    
+
+        
+
+     
